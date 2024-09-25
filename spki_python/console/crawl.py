f8f2273f1de42fa74a398799b2bc02478b44d795
Jason Link <jsn.link@gmail.com>
2024-09-25 14:55:50 -0400

    functional console.crawl module

--- spki_python/console/crawl.py
+++ spki_python/console/crawl.py
@@ -26,60 +26,48 @@ from spki_python.utilities import read_websites_from_file
 
 
 
-def collect_certificates(websites:dict):
-    """_summary_
+def main():
+    """Main function for the console module.
+    """
 
-    Args:
-        websites (dict): _description_
+    args = parse_arguments()
+    if not args:
+        return 1
+
+    # split args.algorithm into a list of digest algorithms
+    digest_algorithms = args.digest_algorithm.split(',')
+
+    if args.nameserver:
+        nameserver = args.nameserver
+    else:
+        nameserver = None
+
+    # Create folders if none exist.
+    os.makedirs(Path(args.dump_folder), exist_ok=True)
+    os.makedirs(Path(f"{args.dump_folder}/certs"), exist_ok=True)
+
+    # Resolve website/domain IPv4 and IPv6 addresses.
+    website_filename = Path(args.input_file)
+    websites = read_websites_from_file(website_filename)
+    resolve_domain_addresses(websites, nameserver=nameserver)
+    website_export_filename = Path(f"{args.dump_folder}/websites.json")
+    save_text_file(website_export_filename, websites)
+
+    certificates = collect_certificates(websites)
+    if certificates:
+        for certificate in certificates:
+            parse_certificate(certificate['Certificate'], digest_algorithms)
+            export_certificate(Path(f"{args.dump_folder}/certs"), certificate)
+        all_certificates_filename = Path(f"{args.dump_folder}/certificates.json")
+        save_text_file(all_certificates_filename, certificates)
+
+
+def parse_arguments():
+    """Parses command-line arguments and returns the parsed arguments.
 
     Returns:
-        _type_: _description_
-    """
-    session_details = []
+        argparse.Namespace: The parsed arguments.
 
-    ip_stack_available = {
-        "A": is_ipv4_available(),
-        "AAAA": is_ipv6_available()
-    }
-
-    for website in websites:
-        logger.info("Accessing online website: %s:%d", website['Domain'], website['Port'])
-        for dns_record in website["DNS Records"]:
-            record_type = list(dns_record.keys())[0]
-            if ip_stack_available[record_type] and dns_record[record_type]:
-                results = get_domain_certificates(
-                    website['Domain'],
-                    dns_record[record_type],
-                    website['Port']
-                    )
-                session_details += results
-
-    return session_details
-
-
-def export_certificate(folder:str, session_record:dict):
-    """_summary_
-
-    Args:
-        certificate (dict): _description_
-    """
-
-    if not session_record["Certificate"]["PEM"]:
-        return False
-
-    for suffix in [".pem", ".der"]:
-        sni_use = "sni_true" if session_record['SNI'] else "sni_false"
-        filename = f"{session_record['Domain']}_{session_record['Address']}_"
-        filename += f"{session_record['Port']}_{sni_use}{suffix}"
-        filepath = Path(f"{folder}/{filename}")
-        save_certificate(filepath, session_record['Certificate']['PEM'])
-
-    return True
-
-
-
-def main():
-    """_summary_
     """
     parser = argparse.ArgumentParser()
     parser.add_argument(
@@ -113,6 +101,14 @@ def main():
         dest='dump_folder',
         help='Base folder to dump all artifacts to.'
     )
+    
+    parser.add_argument(
+        '--digest-algorithm',
+        action='store',
+        default='sha256',
+        dest='digest_algorithm',
+        help='Digest algorithm for the thumbprint calculation.'
+    )
 
     args = parser.parse_args()
 
@@ -133,30 +129,66 @@ def main():
     if not args.input_file and not args.input_folder:
         logger.error("Nothing to do.  Must specify at least one input.")
         return
+    
+    return parser.parse_args()
 
-    if args.nameserver:
-        nameserver = args.nameserver
-    else:
-        nameserver = None
 
-    # Create folders if none exist.
-    os.makedirs(Path(args.dump_folder), exist_ok=True)
-    os.makedirs(Path(f"{args.dump_folder}/certs"), exist_ok=True)
+def collect_certificates(websites:dict):
+    """Collect certificates from websites.
 
-    # Resolve website/domain IPv4 and IPv6 addresses.
-    website_filename = Path(args.input_file)
-    websites = read_websites_from_file(website_filename)
-    resolve_domain_addresses(websites, nameserver=nameserver)
-    website_export_filename = Path(f"{args.dump_folder}/websites.json")
-    save_text_file(website_export_filename, websites)
+    Args:
+        websites (dict): The websites to collect certificates from.
 
-    certificates = collect_certificates(websites)
-    if certificates:
-        for certificate in certificates:
-            parse_certificate(certificate['Certificate'])
-            export_certificate(Path(f"{args.dump_folder}/certs"), certificate)
-        all_certificates_filename = Path(f"{args.dump_folder}/certificates.json")
-        save_text_file(all_certificates_filename, certificates)
+    Returns:
+        list: A list of dictionaries containing the certificates
+              and other details.
+    """
+    session_details = []
+
+    ip_stack_available = {
+        "A": is_ipv4_available(),
+        "AAAA": is_ipv6_available()
+    }
+
+    for website in websites:
+        logger.info("Accessing online website: %s:%d", website['Domain'], website['Port'])
+        for dns_record in website["DNS Records"]:
+            record_type = list(dns_record.keys())[0]
+            if ip_stack_available[record_type] and dns_record[record_type]:
+                results = get_domain_certificates(
+                    website['Domain'],
+                    dns_record[record_type],
+                    website['Port']
+                    )
+                session_details += results
+
+    return session_details
+
+
+
+def export_certificate(folder:str, session_record:dict):
+    """Export certificates to disk in PEM and DER formats.
+
+    Args:
+        folder (str): Path to folder to save certificates.
+        session_record (dict): The session record containing the certificate.
+
+    Returns:
+        bool: True if the certificate was saved, False otherwise.
+    """
+
+
+    if not session_record["Certificate"]["PEM"]:
+        return False
+
+    for suffix in [".pem", ".der"]:
+        sni_use = "sni_true" if session_record['SNI'] else "sni_false"
+        filename = f"{session_record['Domain']}_{session_record['Address']}_"
+        filename += f"{session_record['Port']}_{sni_use}{suffix}"
+        filepath = Path(f"{folder}/{filename}")
+        save_certificate(filepath, session_record['Certificate']['PEM'])
+
+    return True
 
 
 if __name__ == '__main__':
